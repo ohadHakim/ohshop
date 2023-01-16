917ae71fb22539b9cd379b60cbaec213d584fa5a
Ohad Hakim <hakimohad@gmail.com>
2023-01-16 22:41:49 +0200

    adding single product component, and backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,30 @@ import Header from "./components/Header"
 import Footer from "./components/Footer"
 import { Container } from "react-bootstrap"
 import HomeScreen from "./screens/HomeScreen"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import ProductScreen from "./screens/ProductScreen"
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route
+              path='/'
+              element={<HomeScreen />}
+              exact
+            />
+            <Route
+              path='/product/:id'
+              element={<ProductScreen />}
+            />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   )
 }
 
