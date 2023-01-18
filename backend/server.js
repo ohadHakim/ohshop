857450d166775f78dc20f54db20bbfa585500479
Ohad Hakim <hakimohad@gmail.com>
2023-01-18 21:09:51 +0200

    connecting to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
-const express = require("express")
-const products = require("./data/products")
-const dotenv = require("dotenv")
+import express from "express"
+import dotenv from "dotenv"
+import products from "./data/products.js"
+import connectDB from "./config/db.js"
 
 dotenv.config()
+
+connectDB()
 
 const app = express()
 
@@ -13,7 +16,7 @@ app.get("/api/products", (req, res) => {
   res.json(products)
 })
 app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
+  const product = find((p) => p._id === req.params.id)
   res.json(product)
 })
 
